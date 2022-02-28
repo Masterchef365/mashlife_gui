7415507976b1a815bd39dcdb32962f01bfa1775f
Masterchef365 <duncan.freeman1@gmail.com>
2022-02-27 22:15:32 -0800

    Life!

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,23 @@
 use eframe::{egui, epi};
 use egui::{Pos2, Rect, Vec2};
 use std::collections::HashSet;
+use mashlife::{HashLife, Rules, Handle};
+use std::path::Path;
+use anyhow::{Result, Context};
+
+const GRID_SIZE: Vec2 = Vec2::splat(400.);
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
+pub struct MashlifeGui {
     grid_view: GridView,
+    life: HashLife,
+    input: Handle,
+    time_step: usize,
 }
 
-impl Default for TemplateApp {
+impl Default for MashlifeGui {
     fn default() -> Self {
         let mut grid = Grid::new();
 
@@ -21,14 +29,41 @@ impl Default for TemplateApp {
                 }
             }
         }
+        let mut life = HashLife::new("B3/S23".parse().unwrap());
+        let input = load_rle("mashlife/life/52513m.rle", &mut life).unwrap();
 
-        let grid_view = GridView::from_grid(grid);
+        let mut instance = Self { 
+            grid_view: GridView::from_grid(grid),
+            input,
+            life,
+            time_step: 0,
+        };
 
-        Self { grid_view }
+        instance.copy_grid(input);
+
+        instance
     }
 }
 
-impl epi::App for TemplateApp {
+impl MashlifeGui {
+    fn copy_grid(&mut self, handle: Handle) {
+        self.grid_view.grid.clear();
+
+        let rect = self.grid_view.viewbox_grid(GRID_SIZE);
+
+        let mut set_grid = |(x, y)| { let _ = self.grid_view.grid.insert((x as _, y as _)); };
+
+        let rect = (
+            (rect.min.x.floor() as _, rect.min.x.floor() as _),
+            (rect.min.y.ceil() as _, rect.min.y.ceil() as _),
+        );
+
+
+        self.life.resolve((0, 0), &mut set_grid, rect, handle);
+    }
+}
+
+impl epi::App for MashlifeGui {
     fn name(&self) -> &str {
         "eframe template"
     }
@@ -59,6 +94,11 @@ impl epi::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &epi::Frame) {
+        let handle = self.life.result(self.input, self.time_step, (0, 0), 0);
+        self.copy_grid(handle);
+
+        self.time_step += 1;
+
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("eframe template");
             ui.hyperlink("https://github.com/emilk/eframe_template");
@@ -70,7 +110,7 @@ impl epi::App for TemplateApp {
 
             // The central panel the region left after adding TopPanel's and SidePanel's
             //let t = std::time::Instant::now();
-            ui.add(grid_square(&mut self.grid_view, Vec2::splat(400.)));
+            ui.add(grid_square(&mut self.grid_view, GRID_SIZE));
             //dbg!(t.elapsed().as_secs_f32());
         });
     }
@@ -141,6 +181,7 @@ impl GridView {
         Self::from_grid(Grid::new())
     }
 
+    /// Create a new instance from a grid
     pub fn from_grid(grid: Grid) -> Self {
         Self {
             scale: 50.,
@@ -149,10 +190,12 @@ impl GridView {
         }
     }
 
+    /// Handle a drag action
     pub fn drag(&mut self, delta: Vec2) {
         self.center -= delta / self.scale;
     }
 
+    /// Handle a zoom action
     pub fn zoom(&mut self, delta: f32, cursor_px: Pos2, view_size_px: Vec2) {
         self.scale += delta * self.scale;
 
@@ -163,6 +206,7 @@ impl GridView {
         self.center += cursor_off_grid * delta;
     }
 
+    /// Handle a click
     pub fn click(&mut self, cursor_px: Pos2, view_size_px: Vec2) {
         let view_center_px = view_size_px / 2.;
         let cursor_off_px = cursor_px - view_center_px;
@@ -181,11 +225,19 @@ impl GridView {
         }
     }
 
-    pub fn view(&self, view_size_px: Vec2) -> impl Iterator<Item = Rect> + '_ {
+    /// The current view rect, in grid space
+    pub fn viewbox_grid(&self, view_size_px: Vec2) -> Rect {
         let view_center_px = view_size_px / 2.;
         let view_size_grid = view_size_px / self.scale;
 
-        let view_rect_grid = Rect::from_center_size(self.center, view_size_grid);
+        Rect::from_center_size(self.center, view_size_grid)
+    }
+
+    /// Return the rectangles of the pixels which are in view
+    pub fn view(&self, view_size_px: Vec2) -> impl Iterator<Item = Rect> + '_ {
+        let view_center_px = view_size_px / 2.;
+
+        let view_rect_grid = self.viewbox_grid(view_size_px);
 
         self.grid.iter().filter_map(move |&(x, y)| {
             let pos_grid = Pos2::new(x as f32, y as f32);
@@ -198,4 +250,38 @@ impl GridView {
             })
         })
     }
+}
+
+fn load_rle(path: impl AsRef<Path>, life: &mut HashLife) -> Result<Handle> {
+    // Load RLE
+    let (rle, rle_width) =
+        mashlife::io::load_rle(path).context("Failed to load RLE file")?;
+    let rle_height = rle.len() / rle_width;
+
+    let max_rle_dim = rle_height.max(rle_width);
+
+    eprintln!("REMOVE THESE DEFAULTS!!");
+    let expected_steps = 100 as u64 + 12_000;
+    let n = highest_pow_2(max_rle_dim as _)
+        .max(highest_pow_2(expected_steps) + 2);
+
+    let half_width = 1 << n - 1;
+    let quarter_width = 1 << n - 2;
+    let insert_tl = (
+        (half_width - rle_width as i64) / 2 + quarter_width,
+        (half_width - rle_height as i64) / 2 + quarter_width,
+    );
+
+    let start = std::time::Instant::now();
+    let input_cell = life.insert_array(&rle, rle_width, insert_tl, n as _);
+    println!(
+        "Input insertion took {}ms",
+        start.elapsed().as_secs_f32() * 1e3
+    );
+
+    Ok(input_cell)
+}
+
+fn highest_pow_2(v: u64) -> u32 {
+    8 * std::mem::size_of_val(&v) as u32 - v.leading_zeros()
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    let app = mashlife_gui::TemplateApp::default();
+    let app = mashlife_gui::MashlifeGui::default();
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(Box::new(app), native_options);
 }
